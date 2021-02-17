e25afeb34460dead34a45eb5caf6504f1bbbb738
Leyton Nightingale <67469616+ley-288@users.noreply.github.com>
2021-02-17 16:10:24 +0100

    OK

---
+++ resources/views/widgets/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="row">
+        <div class="col-sm-6">
+        </div>
+        <div class="col-sm-6 text-right">
+            Spidergain
+        </div>
+    </div>
+</div>
